7512a436f2c146f9e91f35e11ea66be27a5d33f3
Vamsi Mandhapati <vamsimandhapati7@gmail.com>
2026-07-07 16:54:28 -0400

    Implement audit service

---
+++ account-service/AuditServiceImpl.java
@@ -0,0 +1,12 @@
+package com.banking.ai.account.audit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditServiceImpl implements AuditService {
+
+    @Override
+    public void logAction(String action, String performedBy) {
+
+    }
+}
